d8109c3ac5acf21e8aeba88a08e6f01246bf3fa8
ravi barla <ravibarla1@gmail.com>
2023-09-16 10:26:44 +0530

    adding something

--- src/component/App.js
+++ src/component/App.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import HabitList from "./HabitList";
 import CreateHabit from "./CreateHabit";
 import HabitTracker from "./HabitTracker";
+import Page404 from "./Page404"
 function App() {
   return (
     <div className="App d-flex justify-content-center ">
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" element={<HabitList />} />
           <Route exact path="tracker/:habitId" element={<HabitTracker />} />
           <Route exact path="create" element={<CreateHabit />} />
+          <Route path="*" element={<Page404/>}/>
         </Routes>
       </Router>
     </div>
